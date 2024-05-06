ba856522595e531c837aa8da9e8a81716b0afaed
Igor Gomes <igor.gomes@windifferent.com>
2024-05-06 12:00:00 -0300

    2.05 - Variables - Primary Types

--- basic/variables.tf
+++ basic/variables.tf
@@ -1,3 +1,10 @@
 variable "file_content" {
-  default = "Esse valor é um valor default"
+  default = "Conteúdo default"
+  description = "Essa variável representa o valor a ser salvo no arquivo."
+  type = string
+}
+
+variable "var_bool" {
+  default = false
+  type = bool
 }
