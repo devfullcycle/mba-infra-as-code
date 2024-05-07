7c4a0eccec458ac0ac2de6725b77e9a844f96c07
Igor Gomes <igor.gomes@windifferent.com>
2024-05-07 08:41:12 -0300

    2.06 - Variables - Complex types

--- basic/variables.tf
+++ basic/variables.tf
@@ -8,3 +8,32 @@ variable "var_bool" {
   default = false
   type = bool
 }
+
+variable "fruits" {
+  type    = list(string)
+  default = ["apple", "banana", "apple"]
+}
+
+variable "person_map" {
+  type    = map(string)
+  default = {
+    name = "Igor"
+    age  = 28
+  }
+}
+
+variable "person_tuple" {
+  type    = tuple([ string, number ])
+  default = ["Igor", 28]
+}
+
+variable "person" {
+  type    = object({
+    name  = string
+    age   = number
+  })
+  default = {
+    name = "Igor"
+    age  = 28
+  }
+}
