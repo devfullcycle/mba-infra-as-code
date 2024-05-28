9201fdcd78726e27ed02c8f5219184743bfed630
Igor Gomes <igor.gomes@windifferent.com>
2024-05-28 09:26:18 -0300

    4.04 - Cluster module creation

--- aws/main.tf
+++ aws/main.tf
@@ -1,45 +1,14 @@
 module "network" {
-  source = "./modules/network"
-  vpc_cidr_block = var.vpc_cidr_block
+  source             = "./modules/network"
+  vpc_cidr_block     = var.vpc_cidr_block
   subnet_cidr_blocks = var.subnet_cidr_blocks
-  prefix = var.prefix
+  prefix             = var.prefix
 }
 
-data "aws_secretsmanager_secret" "secret" {
-  arn = "arn:aws:secretsmanager:us-west-2:304851244121:secret:prod/Terraform/Db-3vtdtk"
+module "cluster" {
+  source             = "./modules/cluster"
+  prefix             = var.prefix
+  subnet_id          = module.network.subnet_ids[0]
+  instance_count     = var.instance_count
+  security_group_ids = [module.network.security_group_id]
 }
-
-data "aws_secretsmanager_secret_version" "current" {
-  secret_id = data.aws_secretsmanager_secret.secret.id
-}
-
-resource "aws_instance" "example_instance" {
-  ami                    = "ami-01cd4de4363ab6ee8"
-  instance_type          = "t2.micro"
-  subnet_id              = module.network.subnet_ids[0]
-  vpc_security_group_ids = [module.network.security_group_id]
-
-  user_data = <<EOF
-#!/bin/bash
-DB_STRING="Server=${jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["Host"]};DB=${jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["DB"]}"
-echo $DB_STRING > test.txt
-EOF
-}
-
-
-# resource "aws_eip" "example_ip" {
-#   instance   = aws_instance.example_instance.id
-#   depends_on = [aws_internet_gateway.example_igw]
-# }
-
-# resource "aws_ssm_parameter" "parameter" {
-#   name  = "vm_ip"
-#   type  = "String"
-#   value = aws_eip.example_ip.public_ip
-# }
-
-
-
-
-
-
