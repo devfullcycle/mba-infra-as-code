627da01d33100cce0324948d0f7061e5fe263644
Igor Gomes <igor.gomes@windifferent.com>
2024-05-29 10:06:30 -0300

    4.06 - Auto Scaling Group creation

--- aws/modules/cluster/main.tf
+++ aws/modules/cluster/main.tf
@@ -7,32 +7,42 @@ data "aws_secretsmanager_secret_version" "current" {
   secret_id = data.aws_secretsmanager_secret.secret.id
 }
 
-resource "aws_instance" "instances" {
-  count                  = var.instance_count
-  ami                    = "ami-01cd4de4363ab6ee8"
-  instance_type          = "t2.micro"
-  subnet_id              = var.subnet_id
-  vpc_security_group_ids = var.security_group_ids
+resource "aws_launch_template" "template" {
+  name          = "${var.prefix}-template"
+  image_id      = "ami-01cd4de4363ab6ee8"
+  instance_type = "t2.micro"
 
-  user_data = <<EOF
+  user_data = base64encode(
+    <<EOF
 #!/bin/bash
 DB_STRING="Server=${jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["Host"]};DB=${jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["DB"]}"
 echo $DB_STRING > test.txt
 EOF
+  )
 
-  tags = {
-    "Name" = "${var.prefix}-node-${count.index}"
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = var.security_group_ids
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      "Name" = "${var.prefix}-node"
+    }
   }
 }
 
+resource "aws_autoscaling_group" "asg" {
+  name                = "${var.prefix}-asg"
+  desired_capacity    = 2
+  min_size            = 1
+  max_size            = 3
+  vpc_zone_identifier = var.subnet_ids
 
-# resource "aws_eip" "example_ip" {
-#   instance   = aws_instance.instance.id
-#   depends_on = [aws_internet_gateway.example_igw]
-# }
+  launch_template {
+    id      = aws_launch_template.template.id
+    version = "$Latest"
+  }
+}
 
-# resource "aws_ssm_parameter" "parameter" {
-#   name  = "vm_ip"
-#   type  = "String"
-#   value = aws_eip.example_ip.public_ip
-# }
