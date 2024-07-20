0590364b23900ec609527352faed92278b8fd96a
Igor Gomes <igor.gomes@windifferent.com>
2024-07-20 17:10:22 -0300

    Automated tests

--- aws/environments/prod/main.tf
+++ aws/environments/prod/main.tf
@@ -1,12 +1,12 @@
 module "network" {
-  source             = "../modules/network"
+  source             = "../../modules/network"
   vpc_cidr_block     = var.vpc_cidr_block
   subnet_cidr_blocks = var.subnet_cidr_blocks
   prefix             = var.prefix
 }
 
 module "cluster" {
-  source             = "../modules/cluster"
+  source             = "../../modules/cluster"
   prefix             = var.prefix
   subnet_ids         = module.network.subnet_ids
   security_group_ids = [module.network.security_group_id]
